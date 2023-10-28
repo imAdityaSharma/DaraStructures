350a61168373ef17512aad19ad9b60a8643eaa99
aditya m sharma <adityambha1999@gmail.com>
2023-10-28 23:10:36 +0530

    p1

---
+++ Array/Searching techniques/binarySearch.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+bool BinarySearch(int arr[], int Low, int High){
+    
+}
+int main(){
+    int arr[10]={1,2,3,4,5,6,7,8,9,0};
+    int val = 99;
+    if(BinarySearch(arr,val,10))
+    cout<<"value "<<val<<" is present in array"<<endl;
+    else
+    cout<<"value "<<val<<" is not present in array"<<endl;
+    return 0;
+}
