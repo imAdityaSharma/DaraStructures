c3e52155d0ba4b0d1d822c848729ecef900a5b95
aditya <adityambha1999@gmail.com>
2023-11-23 23:32:51 +0530

    l

--- linkedLists/Singlylist.cpp
+++ linkedLists/Singlylist.cpp
@@ -8,58 +8,14 @@ class LinkedList {
                     private:
                         Node* head;
                     public:
-                    LinkedList() {
-                        head = nullptr; }
-                    void insertAtHead(int data){
-                            Node* newNode = new Node;
-                            newNode->data = data;
-                            newNode->next = head;
-                            head = newNode;
-                        }
-
-                    void insertAtTail(int data) {
-                            if (head == nullptr) {  
-                                insertAtHead(data);
-                                return;                        }   
-                            Node* temp = head;
-                            while (temp->next != nullptr) {
-                                    temp = temp->next;    }
-                            Node* newNode = new Node;
-                            newNode->data = data;
-                            newNode->next = nullptr;
-                            temp->next = newNode;
-                        }
-
-                    void deleteAtHead() {
-                            if (head == nullptr) {
-                                cout << "Linked list is empty." << endl;
-                                return;    }
-                            Node* temp = head;
-                            head = head->next;
-                            delete temp;}
-                    void deleteAtTail() {
-                            if (head == nullptr) {
-                                cout << "Linked list is empty." << endl;
-                                return;   }
-                            if (head->next == nullptr) {
-                                deleteAtHead();
-                                return    ;     }
-                            Node* temp = head;
-                            Node* prev = nullptr;
-                            while (temp->next != nullptr) {
-                                prev = temp;
-                                temp = temp->next;  }
-                            prev->next = nullptr;
-                            delete temp;
-                        }
-                    void printList(){
-                            Node* temp = head;
-                            while (temp != nullptr) {
-                                cout << temp->data << " ";
-                                temp = temp->next;}
-                                cout << endl;
-                        }
+                    LinkedList() { head = nullptr; }
+                    void insertAtHead(int data);
+                    void insertAtTail(int data);
+                    void deleteAtHead();
+                    void deleteAtTail();
+                    void printList();
             };
+
 int main(){
     LinkedList list;
     list.insertAtHead(10);
@@ -75,4 +31,48 @@ int main(){
     cout << "After deleting at tail: ";
     list.printList();
     return 0;
+}
+
+void LinkedList:: insertAtTail(int data) {
+    if (head == nullptr) {  
+            insertAtHead(data);
+            return;                        }   
+            Node* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;    }
+            Node* newNode = new Node;
+            newNode->data = data;
+            newNode->next = nullptr;
+            temp->next = newNode;
+    }
+
+void LinkedList:: deleteAtHead() {
+    if (head == nullptr) {
+        cout << "Linked list is empty." << endl;
+        return;    }
+    Node* temp = head;
+    head = head->next;
+    delete temp;}
+
+void LinkedList:: deleteAtTail() {
+    if (head == nullptr) {
+        cout << "Linked list is empty." << endl;
+        return;   }
+    if (head->next == nullptr) {
+        deleteAtHead();
+        return    ;     }
+    Node* temp = head;
+    Node* prev = nullptr;
+    while (temp->next != nullptr) {
+        prev = temp;
+        temp = temp->next;  }
+    prev->next = nullptr;
+    delete temp;
+}
+void LinkedList:: printList(){
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;}
+    cout << endl;
 }
